3693613849084b4eed7438abd83541252715be9d
AHMED80XX <ahmednasser102030@yahoo.com>
2026-02-16 17:06:48 +0400

    first commit

---
+++ erp-app/src/main/java/com/ultrasystem/erp/ErpUltraSystemApplication.java
@@ -0,0 +1,11 @@
+package com.ultrasystem.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpUltraSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ErpUltraSystemApplication.class, args);
+    }
+}
---
+++ src/test/java/com/ultrasystem/erp/ErpUltrasystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ultrasystem.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpUltrasystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
